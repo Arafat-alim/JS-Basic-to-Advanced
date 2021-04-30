5d0d210967ebb98cbf337b842c75bba9eef52c33
Arafat <arafat97alim@gmail.com>
2021-04-30 17:35:32 +0530

    Adding json object file

---
+++ httpRequest/jSON/jSObjVsJSONObj.js
@@ -0,0 +1,36 @@
+console.log("processing");
+
+// JAVASCRIPT object
+var jsObj = {
+    name: "Arafat",
+    profession: "Engg.",
+    age: 21
+}
+
+//jSON object
+var jSONObj = {
+    "name": "Aman",
+    "profession": "Hacker",
+    "age": 21
+}
+//javascript and JSON Array
+var mArr = [ 1, 2 , 3, 4, 5 ];
+
+//javascript and JSON support Array same object including syntax
+var mArrObj = [{
+    "name": "Aman",
+    "profession": "Hacker",
+    "age": 21
+}]
+
+var mArrObj2 = [{
+    name: "Arafat",
+    profession: "Engg.",
+    age: 21
+}]
+
+console.log(jsObj); //javascript object
+console.log(jSONObj); //JSON object
+console.log(mArr[4]); //jS and JSON array
+console.log(mArrObj) //JSPN array Object
+console.log(mArrObj2); //JS array object 
